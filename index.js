921eb743280839896454a2e12a2e41f9e5ccdd8c
Andy Burke <aburke@bitflood.org>
2017-06-07 19:08:26 -0700

    refactoring, renaming

--- index.js
+++ index.js
@@ -5,14 +5,20 @@ const extend = require( 'extend' );
 
 const Rethink_Helper = module.exports = {};
 
-Rethink_Helper.init = function( options, callback ) {
+Rethink_Helper.init = function( _options, callback ) {
+    const options = extend( {
+        host: 'localhost',
+        port: 28015
+    }, _options );
+
     const result = {};
+
     async.series( [
         _verify.bind( null, options, result ),
         _connect.bind( null, options, result ),
-        _createDB.bind( null, options, result ),
-        _createTables.bind( null, options, result ),
-        _ensureIndexes.bind( null, options, result )
+        _create_database.bind( null, options, result ),
+        _create_tables.bind( null, options, result ),
+        _ensure_indexes.bind( null, options, result )
     ], error => {
         callback( error, result );
     } );
@@ -30,12 +36,12 @@ function _verify( options, result, callback ) {
 function _connect( options, result, callback ) {
     result.r = require( 'rethinkdb' );
 
-    var retries = 0;
+    let retries = 0;
     ( function __connect() {
         result.r.connect( {
-            host: options.host || 'rethinkdb',
-            port: options.port || 28015
-        }, function( error, connection ) {
+            host: options.host,
+            port: options.port
+        }, ( error, connection ) => {
             if ( error && error.name === 'ReqlDriverError' && error.message.indexOf( 'Could not connect' ) === 0 && ++retries < 31 ) {
                 setTimeout( __connect, 1000 );
                 return;
@@ -47,93 +53,96 @@ function _connect( options, result, callback ) {
     } )();
 }
 
-function _createDB( options, result, callback ) {
-    result.r.dbList().contains( options.database ).do( function( dbExists ) {
-        return result.r.branch(
-            dbExists, {
-                created: 0
-            },
-            result.r.dbCreate( options.database )
-        );
-    } ).run( result.connection, callback );
+function _create_database( options, result, callback ) {
+    if ( !result || !result.r || !result.connection ) {
+        callback( 'Must have a rethink instance and a valid connection!' );
+        return;
+    }
+
+    result.r
+        .dbList()
+        .contains( options.database )
+        .do( db_exists => {
+            return result.r
+                .branch(
+                    db_exists,
+                    { created: 0 },
+                    result.r.dbCreate( options.database )
+                );
+        } ).run( result.connection, callback );
 }
 
-function _createTables( options, result, callback ) {
-    async.series( [
-        // check if we even need to do anything
-        function( next ) {
-            next( options.tables ? null : 'skip' );
-        },
+function _create_tables( options, result, callback ) {
+    if ( !result || !result.r || !result.connection ) {
+        callback( 'Must have a rethink instance and a valid connection!' );
+        return;
+    }
 
-        // iterate over tables, creating them as necessary
-        function( next ) {
-            async.eachSeries( options.tables, function( _table, _next ) {
-                var _tableOptions = typeof _table === 'object' ? _table.options || {} : {};
-                var table = typeof _table === 'object' ? _table.name : _table;
+    if ( !options.tables ) {
+        callback();
+        return;
+    }
 
-                var tableOptions = extend( {
-                    shards: 3
-                }, _tableOptions );
+    async.eachSeries( options.tables, ( _table, next ) => {
+        var _table_options = typeof _table === 'object' ? _table.options || {} : {};
+        var table = typeof _table === 'object' ? _table.name : _table;
 
-                result.r.db( options.database ).tableList().contains( table ).do( function( tableExists ) {
-                    return result.r.branch(
-                        tableExists, {
-                            created: 0
-                        },
-                        result.r.db( options.database ).tableCreate( table, tableOptions )
-                    );
-                } ).run( result.connection, _next );
-            }, next );
-        }
-    ], function( error ) {
-        error = error === 'skip' ? null : error;
-        callback( error );
-    } );
+        var table_options = extend( {
+            shards: 3
+        }, _table_options );
+
+        result.r
+            .db( options.database )
+            .tableList()
+            .contains( table )
+            .do( table_exists => {
+                return result.r
+                    .branch(
+                        table_exists,
+                        { created: 0 },
+                        result.r.db( options.database ).tableCreate( table, table_options ) );
+            } ).run( result.connection, next );
+    }, callback );
 }
 
-function _ensureIndexes( options, result, callback ) {
+function _ensure_indexes( options, result, callback ) {
+    if ( !options.indexes ) {
+        callback();
+        return;
+    }
 
     async.series( [
-        // check if we need to do anything
-        function( next ) {
-            next( options.indexes ? null : 'skip' );
-        },
-
         // iterate over indexes, creating them
         function( next ) {
-            async.eachSeries( options.indexes, function( indexSpec, _next ) {
+            async.eachSeries( options.indexes, ( index_spec, _next ) => {
                 result.r
                     .db( options.database )
-                    .table( indexSpec.table )
+                    .table( index_spec.table )
                     .indexList()
-                    .contains( indexSpec.name )
-                    .do( function( indexExists ) {
-                        return result.r.branch(
-                            indexExists, {
-                                created: 0
-                            },
-                            result.r.db( options.database ).table( indexSpec.table ).indexCreate( indexSpec.name, indexSpec.create )
-                        );
+                    .contains( index_spec.name )
+                    .do( index_exists => {
+                        return result.r
+                            .branch(
+                                index_exists,
+                                { created: 0 },
+                                result.r.db( options.database ).table( index_spec.table ).indexCreate( index_spec.name, index_spec.create ) );
                     } ).run( result.connection, _next );
             }, next );
         },
 
         next => {
-            async.eachSeries( options.indexes, ( indexSpec, _next ) => {
-                if ( indexSpec.wait === false ) {
+            async.eachSeries( options.indexes, ( index_spec, _next ) => {
+                if ( index_spec.wait === false ) {
                     _next();
                     return;
                 }
 
                 result.r
                     .db( options.database )
-                    .table( indexSpec.table )
-                    .indexWait( indexSpec.name )
+                    .table( index_spec.table )
+                    .indexWait( index_spec.name )
                     .run( result.connection, _next );
             }, next );
         }
-    ], function( error ) {
-        error = error === 'skip' ? null : error;
-        callback( error );
-    } );
+    ], callback );
 }
